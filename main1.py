4409bf1fcb95edc691a000c9704749ef5371c2bc
Kerin Pithawala <kerinpithawala7@gmail.com>
2021-01-13 12:57:33 +0000

    Added scattering plot for 2 parameter

--- main1.py
+++ main1.py
@@ -7,8 +7,6 @@ df = pd.read_csv('sobar-72.csv')
 
 #df.plot('behavior_eating','attitude_consistency','attitude_spontaneity')
 #scatter-plot for behaviour eating with cervix cancer
-df.plot(kind = 'scatter', x = 'behavior_eating', y = 'ca_cervix')
+df.plot(kind = 'scatter', x = 'attitude_consistency', y = 'ca_cervix')
 
 mp.show()
-
-
